2c3630e85a9a2531aedf6d4ebf437ab1fa91d41f
Yanchenko Serhii <run2cb@gmail.com>
2020-03-13 14:05:04 +0200

    change helm release

--- src/main/java/SpringBootHelloWorld.java
+++ src/main/java/SpringBootHelloWorld.java
@@ -11,7 +11,7 @@ public class SpringBootHelloWorld {
 
     @RequestMapping("/")
     String home() {
-        return "Mood my foot 9!!" + appVersion ;
+        return "Mood my foot 10!!" + appVersion ;
     }
 
     public static void main(String[] args) throws Exception {
