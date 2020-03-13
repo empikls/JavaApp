35689ddc12c851925253c58d4122eab6c2421939
Yanchenko Serhii <run2cb@gmail.com>
2020-03-13 14:33:15 +0200

    edit template

--- src/main/java/SpringBootHelloWorld.java
+++ src/main/java/SpringBootHelloWorld.java
@@ -11,7 +11,7 @@ public class SpringBootHelloWorld {
 
     @RequestMapping("/")
     String home() {
-        return "Mood my foot 10!!" + appVersion ;
+        return "Mood my foot 11!!" + appVersion ;
     }
 
     public static void main(String[] args) throws Exception {
