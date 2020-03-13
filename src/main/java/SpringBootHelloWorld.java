f7a3427ba45d647e457d28c5219d57e733bc553f
Yanchenko Serhii <run2cb@gmail.com>
2020-03-13 13:02:35 +0200

    edit JAva app

--- src/main/java/SpringBootHelloWorld.java
+++ src/main/java/SpringBootHelloWorld.java
@@ -11,7 +11,7 @@ public class SpringBootHelloWorld {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World 3!" + appVersion ;
+        return "Hello World 4!" + appVersion ;
     }
 
     public static void main(String[] args) throws Exception {
