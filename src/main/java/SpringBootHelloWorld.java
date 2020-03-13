664b25ee569fe197698251b52983027d7731a704
Yanchenko Serhii <run2cb@gmail.com>
2020-03-13 12:39:42 +0200

    edit JavaApp

--- src/main/java/SpringBootHelloWorld.java
+++ src/main/java/SpringBootHelloWorld.java
@@ -1,20 +1,21 @@
 package net.codejava;
- 
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
- 
+
 @RestController
 @EnableAutoConfiguration
 public class SpringBootHelloWorld {
- 
+    String appVersion = System.getenv("INPUT_VERSION");
+
     @RequestMapping("/")
     String home() {
-        return "Hello World 13!";
+        return "Hello World 13!" + appVersion ;
     }
- 
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootHelloWorld.class, args);
     }
- 
+
 }
