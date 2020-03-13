f9e3148549a9e27da5192a0d5256d966fc6cd8de
Yanchenko Serhii <run2cb@gmail.com>
2020-03-13 15:28:58 +0200

    add some changes

--- src/main/java/SpringBootHelloWorld.java
+++ src/main/java/SpringBootHelloWorld.java
@@ -11,7 +11,7 @@ public class SpringBootHelloWorld {
 
     @RequestMapping("/")
     String home() {
-        return "Mood my foot 13!!" + appVersion ;
+        return "Mood my foot 13!!!" + appVersion ;
     }
 
     public static void main(String[] args) throws Exception {
